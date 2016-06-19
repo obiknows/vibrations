9ad78996beaad4c6ee4e207bfb797983c5f2e7bb
Sam Nnodim <sam.nnodim@gmail.com>
2016-06-19 14:04:55 -0400

    wrote main.js from electron quik start

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
 const electron = require('electron');
 // module that controls app lifecycle
 const {app} = electron;
-// 
+// module to create native window
+const {BrowserWindow} = electron;
+
+// global reference of the window obj (for grbge coll. reasons)
+let win;
+
+function createWindow() {
+  // create new window
+  win = new BrowserWindow({width: 500, height: 500});
+  // load index.html
+  win.loadURL(`file://${__dirname}/index.html`);
+
+  // handle window close
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+// READY - when the app is ready..call this
+app.on('ready', createWindow);
+
+// WHEN ALL WINDOWS CLOSED
+app.on('window-all-closed', () => {
+  // if you're on mac, keep open unless user quits w/ Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
+// ON REOPEN
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
